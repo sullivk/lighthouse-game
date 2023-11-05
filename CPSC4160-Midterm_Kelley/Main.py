5d6f4d8c0b61029159dc0be4a7dacca5fa1db8e5
jacklawrenceclemson <jack.lawrence.122@gmail.com>
2023-11-05 12:46:33 -0500

    first commit

---
+++ CPSC4160-Midterm_Kelley/Main.py
@@ -0,0 +1,116 @@
+import pygame
+import player 
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+FPS = 60
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Midterm - Parallax Effect")
+
+
+#define game variables
+scroll = 0
+
+# Load background images
+bg_images = []
+
+for i in range(4):
+    bg_image = pygame.image.load(f"BG1/IMG_{i}.png").convert_alpha()
+    bg_images.append(bg_image)
+
+bg_width = bg_images[0].get_width()
+
+# Calculate the minimum width of background images
+min_bg_width = min(bg.get_width() for bg in bg_images)
+
+# Initialize the right scrolling limit
+right_scroll_limit = min_bg_width - SCREEN_WIDTH
+
+ground_image = pygame.image.load("BG1/ground.png").convert_alpha()
+	
+ground_width = ground_image.get_width()	
+ground_height = ground_image.get_height()	
+
+#draw background images 
+def draw_bg():
+    global scroll
+    speed = 1
+    for x in range(2):
+        screen.blit(bg_images[x], (int(0 - scroll * speed * 0.25), 0))
+        speed += 0.2
+    for y in range(2,4):
+        screen.blit(bg_images[y], (int(0 - scroll * speed * 0.5), SCREEN_HEIGHT * 0.5))
+
+#draw background
+def draw_ground():
+	for x in range(5):	
+		screen.blit(ground_image, (0, SCREEN_HEIGHT - ground_height))
+
+player = player.Character((20, SCREEN_HEIGHT - ground_height - 250))
+
+# Player height/jump info
+player_y = SCREEN_HEIGHT - ground_height - 250
+player_jump = False
+jump_count = 10
+
+# Game loop
+run = True
+while run:
+    clock.tick(FPS)
+    
+    draw_bg()
+    draw_ground()
+    
+    # Input data. Left and right keys to create the parallax effect
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and scroll > 0:
+        scroll -= 5
+
+    if key[pygame.K_RIGHT] and scroll < right_scroll_limit:
+        scroll += 5
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE] and not player_jump:
+        player_jump = True
+    
+    if player_jump:
+        if jump_count >= -10:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            player_y -= (jump_count ** 2) * 0.5 * neg
+            jump_count -= 1
+        else:
+            player_jump = False
+            jump_count = 10
+
+    if scroll > 0 and scroll < right_scroll_limit: 
+        player.handle_event(event)
+    else: 
+        end_screen = pygame.event.Event(pygame.KEYUP)
+        if scroll == 0: 
+            end_screen.key = pygame.K_RIGHT
+        else:
+            end_screen.key = pygame.K_LEFT
+        player.handle_event(end_screen)
+
+    player.update((20, player_y))
+
+    screen.blit(player.image, player.rect)
+    
+    pygame.display.update()
+
+pygame.quit()
---
+++ CPSC4160-Midterm_Kelley/player.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, position):
+    
+        #load image
+        self.sheet = pygame.image.load("spritesheet2.png")
+        
+        #defines area of a single sprite of an image
+        self.sheet.set_clip(pygame.Rect(0, 0, 200, 275))
+        
+        #loads spritesheet images
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        
+        #position image in the screen surface
+        self.rect.topleft = position
+        
+        #variable for looping the frame sequence
+        self.frame = 0
+        
+        self.rectWidth = 200
+        self.rectHeight = 275
+
+        self.down_states = { }     
+            
+        self.up_states = { }  
+        
+         
+        self.right_states = { 0: (35, 602, self.rectWidth, self.rectHeight), 
+                            1: (0, 0, self.rectWidth,  self.rectHeight), 
+                            2: (330, 0, self.rectWidth,  self.rectHeight), 
+                            3: (660, 0, self.rectWidth,  self.rectHeight),
+                            4: (990, 0, self.rectWidth,  self.rectHeight),
+                            5: (1315, 0, self.rectWidth, self.rectHeight),
+                            6: (1645, 0, self.rectWidth, self.rectHeight) }
+
+        self.left_states = {  0: (256, 602, self.rectWidth, self.rectHeight), 
+                            1: (0, 288, self.rectWidth,  self.rectHeight), 
+                            2: (330, 288, self.rectWidth,  self.rectHeight), 
+                            3: (660, 288, self.rectWidth,  self.rectHeight),
+                            4: (990, 288, self.rectWidth,  self.rectHeight),
+                            5: (1315, 288, self.rectWidth, self.rectHeight),
+                            6: (1645, 288, self.rectWidth, self.rectHeight) }
+
+
+    def get_frame(self, frame_set):
+        #looping the sprite sequences.
+        self.frame += 1
+        
+        #if loop index is higher that the size of the frame return to the first frame 
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        #print(frame_set[self.frame])
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
+
+    def update(self, direction):
+        if direction == 'left':
+            self.clip(self.left_states)
+            #animate rect coordinates
+            self.rect.x -= 5
+        if direction == 'right':
+            self.clip(self.right_states)
+            self.rect.x += 5
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= 5
+        if direction == 'down':
+            self.clip(self.down_states)
+            self.rect.y += 5
+
+        if direction == 'stand_left':
+            self.clip(self.left_states[0])
+        if direction == 'stand_right':
+            self.clip(self.right_states[0])
+        if direction == 'stand_up':
+            self.clip(self.up_states[0])
+        if direction == 'stand_down':
+            self.clip(self.down_states[0])
+
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+
+    def handle_event(self, event):
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                self.update('left')
+            if event.key == pygame.K_RIGHT:
+                self.update('right')
+            if event.key == pygame.K_UP:
+                self.update('up')
+            if event.key == pygame.K_DOWN:
+                self.update('down')
+
+        if event.type == pygame.KEYUP:
+
+            if event.key == pygame.K_LEFT:
+                self.update('stand_left')
+            if event.key == pygame.K_RIGHT:
+                self.update('stand_right')
+            if event.key == pygame.K_UP:
+                self.update('stand_up')
+            if event.key == pygame.K_DOWN:
+                self.update('stand_down')
