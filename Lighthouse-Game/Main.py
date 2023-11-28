79dbff6d08522856f1a7232c726eb225ab399de1
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2023-11-28 17:58:31 -0500

    Update Main.py to most Current Form

--- Lighthouse-Game/Main.py
+++ Lighthouse-Game/Main.py
@@ -31,8 +31,6 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("The Lighthouse - Milestone 3")
 
-
-
 # ============================================
 #                     HUD
 # ============================================
