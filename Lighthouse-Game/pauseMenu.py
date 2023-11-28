01a875292eb484c28c52c511e1f4ec49a6b8f5d5
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2023-11-28 16:37:29 -0500

    Added and Updated Pause Menu with new background

--- Lighthouse-Game/pauseMenu.py
+++ Lighthouse-Game/pauseMenu.py
@@ -1,8 +1,12 @@
 import pygame
+import os
 
 def display_pause_menu(screen):
     paused = True
-
+    
+    pause_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
+    #pause_bg = pygame.transform.scale(pause_bg, (400, 200))
+    
     while paused:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +22,9 @@ def display_pause_menu(screen):
         text = font.render("Game Paused", True, (255, 255, 255))  # White color
         text_rect = text.get_rect(center=(screen.get_width() // 2, 50))
         screen.blit(text, text_rect)
+        
+        image_rect = pause_bg.get_rect(center=(screen.get_width() // 2, 300))
+        screen.blit(pause_bg, image_rect)
 
         pygame.display.update()
 
