84216ff327f45af651fe169e6ced9aa119e7d1f3
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2023-12-11 02:47:35 -0500

    Final Update pauseMenu.py

--- Lighthouse-Game/pauseMenu.py
+++ Lighthouse-Game/pauseMenu.py
@@ -1,9 +1,9 @@
 import pygame
 import os
 
-def display_pause_menu(screen):
+def display_pause_menu(screen, paused):
     paused = True
-    is_settings = False
+    is_controls = False
 
     try:
         pause_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
@@ -13,14 +13,12 @@ def display_pause_menu(screen):
     button_width, button_height = 200, 40
 
     resume_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 150, button_width, button_height)
-    settings_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 200, button_width, button_height)
-    quit_title_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 250, button_width, button_height)
-    quit_game_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 300, button_width, button_height)
+    controls_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 200, button_width, button_height)
+    quit_game_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 250, button_width, button_height)
 
     buttons = [
         {"text": "Resume", "rect": resume_button_rect, "clicked": False},
-        {"text": "Settings", "rect": settings_button_rect, "clicked": False},
-        {"text": "Quit to Title", "rect": quit_title_button_rect, "clicked": False},
+        {"text": "Controls", "rect": controls_button_rect, "clicked": False},
         {"text": "Quit Game", "rect": quit_game_button_rect, "clicked": False}
     ]
     
@@ -35,22 +33,16 @@ def display_pause_menu(screen):
                     if button["rect"].collidepoint(mouse_pos):
                         button["clicked"] = True
                         draw_button(screen, button["text"], button["rect"], button["clicked"], button["hovered"])
-                        paused = handle_button_click(button, screen, paused, is_settings)
+                        paused = handle_button_click(button, screen, paused, is_controls)
     
         # Check if the mouse is over any button and set cursor accordingly
         for button in buttons:
             if button["rect"].collidepoint(pygame.mouse.get_pos()):
                 button["hovered"] = True
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-                #draw_button(screen, button["text"], button["rect"], button["clicked"], button["hovered"])
             else:
                 button["hovered"] = False
-                #pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-                #draw_button(screen, button["text"], button["rect"], button["clicked"], button["hovered"])
 
-        # Draw buttons with hover effect
-        #for button in buttons:
-           #draw_button(screen, button["text"], button["rect"], button["clicked"], button["hovered"])
         
         # Draw your pause menu elements here
         screen.fill((0, 0, 0))
@@ -58,7 +50,7 @@ def display_pause_menu(screen):
         
         # Add "Game Paused" text
         font = pygame.font.Font(None, 36)
-        text = font.render("Game Paused", True, (0, 0, 0))  # Black color
+        text = font.render("Game Paused", True, (255, 255, 255))  # Black color
         text_rect = text.get_rect(center=(screen.get_width() // 2, 50))
         screen.blit(text, text_rect)
         
@@ -79,9 +71,9 @@ def display_pause_menu(screen):
         elif keys[pygame.K_q]:  # Quit game
             pygame.quit()
             quit()
-        elif keys[pygame.K_s]:  # Settings
-            is_settings = True
-            display_settings_menu(screen, is_settings);
+        elif keys[pygame.K_c]:  # Controls
+            is_controls = True
+            display_controls_menu(screen, is_controls);
         #elif keys[pygame.K_t]:  #Back To Title Screen
 
     return paused
@@ -107,21 +99,14 @@ def draw_button(screen, text, rect, clicked, hovered):
     text_rect = text_surface.get_rect(center=rect.center)
     screen.blit(text_surface, text_rect)
     
-def handle_button_click(button, screen, paused, is_settings):
+def handle_button_click(button, screen, paused, is_controls):
     # Handle button click logic
     if button["text"] == "Resume":
         paused = False
-    elif button["text"] == "Settings":
-        # Handle Settings button click (e.g., show controls)
-        is_settings = True
-        display_settings_menu(screen, is_settings)
-    elif button["text"] == "Quit to Title":
-        # Handle Quit to Title Screen button click
-        # Title Menu
-        title_background_image = pygame.image.load("Menu/Title.png")
-        title_menu = True
-        screen.blit(title_background_image, (0, 0))
-        return title_menu
+    elif button["text"] == "Controls":
+        # Handle controls button click (e.g., show controls)
+        is_controls = True
+        display_controls_menu(screen, is_controls)
     elif button["text"] == "Quit Game":
         pygame.quit()
         quit()
@@ -130,10 +115,8 @@ def handle_button_click(button, screen, paused, is_settings):
         
     return paused
     
-def display_settings_menu(screen, is_settings):
-    # Add logic to display settings menu
-    # For example, show controls and a back button
-    settings_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
+def display_controls_menu(screen, is_controls):
+    controls_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
 
     button_width, button_height = 200, 40
     back_button_rect = pygame.Rect(screen.get_width() // 2 - button_width // 2, 150, button_width, button_height)
@@ -141,12 +124,12 @@ def display_settings_menu(screen, is_settings):
     back_button = {"text": "Back", "rect": back_button_rect, "clicked": False, "hovered": False}
 
     controls_info = [
-        "Controls:",
         "Left Arrow Key - Move Left",
         "Right Arrow Key - Move Right",
-        "Up Arrow Key - Jump",
+        "Up Arrow Key - Jump/Climb",
         "Down Arrow Key - Move Down",
         "Right Arrow Key - Move Right",
+        "Space Bar - Punch",
         "P Key - Pause Game",
         "R Key - Resume Game",
         "Q Key - Quit Game",
@@ -155,7 +138,7 @@ def display_settings_menu(screen, is_settings):
     # Draw buttons with hover effect
     draw_button(screen, back_button["text"], back_button["rect"], back_button["clicked"], back_button["hovered"])
     
-    while is_settings:
+    while is_controls:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -173,14 +156,14 @@ def display_settings_menu(screen, is_settings):
         else:
             back_button["hovered"] = False
 
-        # Draw settings menu elements
+        # Draw controls menu elements
         screen.fill((0, 0, 0))
-        settings_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
-        image_rect = settings_bg.get_rect(center=(screen.get_width() // 2, 300))
-        screen.blit(settings_bg, image_rect)
+        controls_bg = pygame.image.load("Prompt Images/resized_pause_menu_bg.png").convert_alpha()
+        image_rect = controls_bg.get_rect(center=(screen.get_width() // 2, 300))
+        screen.blit(controls_bg, image_rect)
         
         font = pygame.font.Font(None, 36)
-        text = font.render("Settings", True, (255, 255, 255))  # White color
+        text = font.render("Controls", True, (255, 255, 255))  # White color
         text_rect = text.get_rect(center=(screen.get_width() // 2, 50))
         screen.blit(text, text_rect)
 
@@ -193,9 +176,6 @@ def display_settings_menu(screen, is_settings):
             screen.blit(controls_text, controls_text_rect)
             controls_y_pos += 30
         
-        #image_rect = settings_bg.get_rect(center=(screen.get_width() // 2, 300))
-        #screen.blit(settings_bg, image_rect)
-
         # Draw back button
         draw_button(screen, back_button["text"], back_button["rect"], back_button["clicked"], back_button["hovered"])
 
